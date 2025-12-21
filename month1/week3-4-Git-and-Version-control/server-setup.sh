6d2a7bc5c6ff0224d867ba55e3755db326134469
NikKnez <nknezevic@live.com>
2025-12-21 18:22:56 +0100

    Add server setup script

---
+++ month1/week3-4-Git-and-Version-control/server-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Server setup automation script
+
+echo "Installing nginx..."
+sudo apt update
+sudo apt install -y nginx
+
+echo "Startting nginx..."
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+echi "Server setup complete!"
